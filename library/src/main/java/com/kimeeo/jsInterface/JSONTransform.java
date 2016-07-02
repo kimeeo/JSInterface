81a928017dcc340b1e809033b1ee75a8997c3366
BhavinPadhiyar <mail.kimeeo@gmail.com>
2016-07-02 13:50:08 +0530

    Initial commit

---
+++ library/src/main/java/com/kimeeo/jsInterface/JSONTransform.java
@@ -0,0 +1,29 @@
+package com.kimeeo.jsInterface;
+
+import android.support.annotation.RawRes;
+
+/**
+ * Created by BhavinPadhiyar on 16/06/16.
+ */
+public class JSONTransform extends JavaScript{
+
+    public JSONTransform(@RawRes int jsFile, android.content.Context context, String[] methods) {
+        super(jsFile,context,methods);
+    }
+
+    public JSONTransform(String javaScriptCode, String[] methods) {
+        super(javaScriptCode,methods);
+    }
+
+    public JSONTransform(@RawRes int jsFile, android.content.Context context, String method) {
+        super(jsFile,context,method);
+    }
+
+    public JSONTransform(String javaScriptCode, String method) {
+        super(javaScriptCode,method);
+    }
+
+    public String transform(String json){
+        return call("php_crud_api_transform",json).toString();
+    };
+}
---
+++ library/src/main/java/com/kimeeo/jsInterface/JavaScript.java
@@ -0,0 +1,110 @@
+package com.kimeeo.jsInterface;
+
+import android.support.annotation.RawRes;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by BhavinPadhiyar on 16/06/16.
+ */
+public class JavaScript {
+    public JavaScript(@RawRes int jsFile, android.content.Context context, String[] methods) {
+        try {
+            InputStream is = context.getResources().openRawResource(jsFile);
+            byte[] buffer = new byte[is.available()];
+            while (is.read(buffer) != -1);
+            String javaScriptCode = new String(buffer);
+            config(javaScriptCode,methods);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JavaScript(String javaScriptCode, String[] methods) {
+        config(javaScriptCode,methods);
+    }
+
+    public void clear() {
+        rhino = null;
+        scope = null;
+        functions = null;
+    }
+
+    public JavaScript(@RawRes int jsFile, android.content.Context context, String method) {
+        try {
+            InputStream is = context.getResources().openRawResource(jsFile);
+            byte[] buffer = new byte[is.available()];
+            while (is.read(buffer) != -1);
+            String javaScriptCode = new String(buffer);
+            config(javaScriptCode,new String[]{method});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public JavaScript(String javaScriptCode, String method) {
+        config(javaScriptCode,new String[]{method});
+    }
+
+    private Context rhino;
+    private Scriptable scope;
+    private Map<String,Function> functions;
+    public void config(String javaScriptCode,String[] methods)
+    {
+        rhino = Context.enter();
+        rhino.setOptimizationLevel(-1);
+        scope = rhino.initStandardObjects();
+        functions = new HashMap<>();
+        for (String method : methods) {
+            Function jsFunction = getFunction(javaScriptCode, method);
+            if (jsFunction != null)
+                functions.put(method, jsFunction);
+        }
+    }
+    public void config(String javaScriptCode,String method)
+    {
+        config(javaScriptCode,new String[]{method});
+    }
+    public void config(@RawRes int jsFile, android.content.Context context,String[] methods)
+    {
+        try {
+            InputStream is = context.getResources().openRawResource(jsFile);
+            byte[] buffer = new byte[is.available()];
+            while (is.read(buffer) != -1) ;
+            String javaScriptCode = new String(buffer);
+            config(javaScriptCode,methods);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void config(@RawRes int jsFile, android.content.Context context,String method)
+    {
+        config(jsFile,context,new String[]{method});
+    }
+    public Function getFunction(String javaScriptCode, String method)
+    {
+        rhino.evaluateString(scope, javaScriptCode, "JavaScript", 1, null);
+        Object obj = scope.get(method, scope);
+        if (obj instanceof Function)
+            return (Function) obj;
+        return null;
+    }
+
+    public Object call(String method,Object... params)
+    {
+        for (Map.Entry<String, Function> entry : functions.entrySet()) {
+            if (entry.getKey().equals(method)) {
+                Function function = entry.getValue();
+                if(function!=null)
+                    return function.call(rhino, scope, scope, params);
+            }
+        }
+        return null;
+    }
+}
